5cd6e314b158ce76d599270c9d1d1ca66b2956b6
Jeff Wood <jeffwood1992@gmail.com>
2022-07-03 00:22:48 -0400

    add console log click tracking for Login and Logout buttons

--- frontend/src/components/auth/Login-Button.js
+++ frontend/src/components/auth/Login-Button.js
@@ -1,10 +1,15 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 import React from "react";
 
 export const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
+    function handleLogin(event) {
+        loginWithRedirect()
+            .then(() => console.log("Login Successful"))
+    }
+
     return (
-        <a className="nav-link" onClick={() => loginWithRedirect()}>Login</a>
+        <a className="nav-link" onClick={handleLogin}>Login</a>
     )
 }
--- frontend/src/components/auth/Logout-Button.js
+++ frontend/src/components/auth/Logout-Button.js
@@ -1,10 +1,17 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 import React from "react";
 
 export const LogoutButton = () => {
     const { logout } = useAuth0();
 
+    function handleLogout(event) {
+        console.log("Logging out user");
+        logout({
+            returnTo: window.location.origin
+        });
+    }
+
     return (
-        <a className="nav-link" onClick={() => logout({returnTo: window.location.origin})}>Logout</a>
+        <a className="nav-link" onClick={handleLogout}>Logout</a>
     )
 }
