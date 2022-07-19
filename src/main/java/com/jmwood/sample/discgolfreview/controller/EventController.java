613e88c5e5bb4fe78254c96339577043dadaf0ad
Jeff Wood <jeffwood1992@gmail.com>
2022-07-19 00:28:49 -0400

    change key used for Kafka topic from User Id to Session Id

--- src/main/java/com/jmwood/sample/discgolfreview/controller/EventController.java
+++ src/main/java/com/jmwood/sample/discgolfreview/controller/EventController.java
@@ -34,7 +34,7 @@ public class EventController {
     public ResponseEntity submitAuthEvent(@RequestBody AuthEvent authEvent) {
         log.info("Received request to submit the following AuthEvent for processing: {}", authEvent);
         AuthEvent persistedEvent = authEventRepository.save(authEvent);
-        authEventKafkaTemplate.send(authEventKafkaTemplate.getDefaultTopic(), authEvent.getUser().getId(), persistedEvent);
+        authEventKafkaTemplate.send(authEventKafkaTemplate.getDefaultTopic(), authEvent.getSessionId(), persistedEvent);
         return ResponseEntity.ok().build();
     }
 
@@ -42,7 +42,7 @@ public class EventController {
     public ResponseEntity submitNavEvent(@RequestBody NavEvent navEvent) {
         log.info("Received request to submit the following NavEvent for processing: {}", navEvent);
         NavEvent persistedEvent = navEventRepository.save(navEvent);
-        navEventKafkaTemplate.send(navEventKafkaTemplate.getDefaultTopic(), navEvent.getUser().getId(), persistedEvent);
+        navEventKafkaTemplate.send(navEventKafkaTemplate.getDefaultTopic(), navEvent.getSessionId(), persistedEvent);
         return ResponseEntity.ok().build();
     }
 
@@ -50,7 +50,7 @@ public class EventController {
     public ResponseEntity submitClickEvent(@RequestBody ClickEvent clickEvent) {
         log.info("Received request to submit the following ClickEvent for processing: {}", clickEvent);
         ClickEvent persistedEvent = clickEventRepository.save(clickEvent);
-        clickEventKafkaTemplate.send(clickEventKafkaTemplate.getDefaultTopic(), clickEvent.getUser().getId(), persistedEvent);
+        clickEventKafkaTemplate.send(clickEventKafkaTemplate.getDefaultTopic(), clickEvent.getSessionId(), persistedEvent);
         return ResponseEntity.ok().build();
     }
 
@@ -58,7 +58,7 @@ public class EventController {
     public ResponseEntity submitCourseEvent(@RequestBody CourseEvent courseEvent) {
         log.info("Received request to submit the following CourseEvent for processing: {}", courseEvent);
         CourseEvent persistedEvent = courseEventRepository.save(courseEvent);
-        courseEventKafkaTemplate.send(courseEventKafkaTemplate.getDefaultTopic(), courseEvent.getUser().getId(), persistedEvent);
+        courseEventKafkaTemplate.send(courseEventKafkaTemplate.getDefaultTopic(), courseEvent.getSessionId(), persistedEvent);
         return ResponseEntity.ok().build();
     }
 }
